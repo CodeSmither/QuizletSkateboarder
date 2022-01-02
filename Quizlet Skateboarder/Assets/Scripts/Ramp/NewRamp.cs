2fba5a09ba5560513155445f20d397d55aded171
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2022-01-02 09:37:56 +0000

    Ramp Decision Zone
    + Lets Player Choose which direction they go up the ramp

---
+++ Quizlet Skateboarder/Assets/Scripts/Ramp/NewRamp.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewRamp : MonoBehaviour
+{
+    Rigidbody skateboardrb;
+    NewSkateboardController newSkateboardController;
+    GameObject skateboard;
+
+    private void Start()
+    {
+        skateboardrb = GameObject.Find("Board").GetComponent<Rigidbody>();
+        newSkateboardController = skateboardrb.gameObject.GetComponent<NewSkateboardController>();
+        skateboard = skateboardrb.gameObject;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Skateboard")
+        {
+            Vector3 rightVec = new Vector3(skateboard.transform.right.x, 0.0f, skateboard.transform.right.z);
+            if (Vector3.Dot(gameObject.transform.right, rightVec.normalized) >= 0.0f && Vector3.Dot(gameObject.transform.right, skateboardrb.velocity.normalized) >= 0.0f)
+            {
+                
+                if (skateboardrb.velocity.magnitude > 4.0f)
+                {
+                    newSkateboardController.controlsDisabled = true;
+                    Invoke("EnableControls", 3.0f);
+                    skateboardrb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+                    skateboardrb.velocity = new Vector3(0f, 10.0f, 0.0f);
+                    skateboardrb.rotation = Quaternion.Euler(0, skateboardrb.rotation.eulerAngles.y, 90);
+                    
+                    
+                }
+            }
+        }
+    }
+
+    private void EnableControls()
+    {
+        newSkateboardController.controlsDisabled = false;
+    }
+
+}
---
+++ Quizlet Skateboarder/Assets/Scripts/Ramp/RampAir.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampAir : MonoBehaviour
+{
+    SkateboardStatus skateboardStatus;
+    private void Start()
+    {
+        skateboardStatus = GameObject.Find("Skateboard").GetComponent<SkateboardStatus>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        skateboardStatus.RampAir = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        skateboardStatus.RampAir = false;
+    }
+}
