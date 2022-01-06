91dd66d0f15fd499de4e761603735bb5bc7b5482
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2022-01-06 15:13:11 +0000

    Work on GrindRail
    Code which checks where train should be

--- Quizlet Skateboarder/Assets/Scripts/GrindRail/GrindRail.cs
+++ Quizlet Skateboarder/Assets/Scripts/GrindRail/GrindRail.cs
@@ -6,23 +6,31 @@ public class GrindRail : MonoBehaviour
 {
     GameObject GrindRailAgent;
     GameObject Skateboard;
+    GameObject GrindRailScan;
+    
     void Start()
     {
         GrindRailAgent = gameObject.transform.GetChild(0).gameObject;
+        GrindRailScan = gameObject.transform.GetChild(1).gameObject;
         Skateboard = GameObject.Find("Board");
     }
 
     private void Update()
     {
-        if (Skateboard.transform.position.x < GrindRailAgent.transform.position.x + 13.5f && GrindRailAgent.transform.position.x - 13.5f < Skateboard.transform.position.x)
+        if (Skateboard.transform.position.x < GrindRailAgent.transform.position.x + 13.5f && GrindRailAgent.transform.position.x - 13.5f < Skateboard.transform.position.x) 
         {
-            float x = Skateboard.transform.position.x;
+            
             if (Skateboard.transform.position.z < GrindRailAgent.transform.position.z + 13.5f && GrindRailAgent.transform.position.z - 13.5f < Skateboard.transform.position.z)
             {
-                float z = Skateboard.transform.position.z;
-
+                Quaternion colliderRotation = GrindRailScan.GetComponent<BoxCollider>().transform.rotation;  
+                Vector3 NearestPoint = Physics.ClosestPoint(GrindRailScan.transform.position,GrindRailScan.GetComponent<BoxCollider>(),GrindRailScan.GetComponent<BoxCollider>().transform.position, colliderRotation);
+                GrindRailAgent.transform.position = NearestPoint;
             }
         }
+        
+            
+            
+        
     }
 
 
