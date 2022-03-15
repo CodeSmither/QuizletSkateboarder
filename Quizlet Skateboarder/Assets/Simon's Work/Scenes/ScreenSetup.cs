e3f2197f31c95c9220ae45cb47226010695962ff
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2022-03-15 22:51:24 +0000

    Main Menu Improvements

---
+++ Quizlet Skateboarder/Assets/Simon's Work/Scenes/ScreenSetup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSetup : MonoBehaviour
+{
+    [SerializeField] List<GameObject> ScreenList = new List<GameObject>();
+    List<bool> ListofActivity = new List<bool>();
+    public void Awake()
+    {
+
+        ScreenList.Add(GameObject.Find("Game Select Panel"));
+        ScreenList.Add(GameObject.Find("Options Panel"));
+        ScreenList.Add(GameObject.Find("Credits Panel"));
+        ScreenList.Add(GameObject.Find("Tutorial Panel"));
+        ScreenList.Add(GameObject.Find("Songs Panel"));
+        foreach(GameObject Screen in ScreenList)
+        {
+            ListofActivity.Add(false);
+            Screen.SetActive(false);
+        }
+    }
+
+    public void ActivateandDeactivate(int DesiredScreenNumber)
+    {
+        ListofActivity[DesiredScreenNumber] = !ListofActivity[DesiredScreenNumber];
+        ScreenList[DesiredScreenNumber].SetActive(ListofActivity[DesiredScreenNumber]);
+    }
+
+}
