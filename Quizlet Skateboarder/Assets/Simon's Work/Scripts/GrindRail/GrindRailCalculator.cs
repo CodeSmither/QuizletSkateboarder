2d12696f7a5ba3d74b78e695cdd47ce2e461a6a3
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2022-03-16 15:08:09 +0000

    Small Upadate
    Small Update

--- Quizlet Skateboarder/Assets/Simon's Work/Scripts/GrindRail/GrindRailCalculator.cs
+++ Quizlet Skateboarder/Assets/Simon's Work/Scripts/GrindRail/GrindRailCalculator.cs
@@ -17,16 +17,21 @@ public class GrindRailCalculator : MonoBehaviour
 
     public void Start()
     {
-        
+        // stores the skateboard Rigidbody
         Skateboardrb = GameObject.Find("Board").GetComponent<Rigidbody>();
+        // stores the skateboard parent object
         GridRail = gameObject.transform.parent.gameObject;
+        // stores both ends of the grindrail
         NorthNode = GameObject.Find("North Node");
         SouthNode = GameObject.Find("South Node");
+        // stores a reference to the skateboard's current status
         skateboardStatus = GameObject.Find("Skateboard").GetComponent<SkateboardStatus>();
+        
     }
 
     private void OnTriggerStay(Collider other)
     {
+        // checks where the skateboard is moving by checking which direction it is relative to
         if (Vector3.Dot(NorthNode.transform.position,Skateboardrb.velocity) > 0)
         {
             Direction = "North";
@@ -37,6 +42,7 @@ public class GrindRailCalculator : MonoBehaviour
             Direction = "South";
             Skateboardrb.transform.rotation = Quaternion.Euler(Skateboardrb.transform.rotation.x, 315f, Skateboardrb.transform.rotation.z);
         }
+        // checks if the skateboard is moving in the correct direction
         if (Vector3.Dot(Skateboardrb.transform.right, GridRail.transform.right) < 0)
         {
             Vector3 LineVector = (NorthNode.transform.position - SouthNode.transform.position).normalized;
@@ -45,6 +51,7 @@ public class GrindRailCalculator : MonoBehaviour
             Destination = SouthNode.transform.position + (LineVector * t);
             Skateboardrb.transform.position = Destination;
         }
+        
         else if (Vector3.Dot(Skateboardrb.transform.right, GridRail.transform.right) > 0)
         {
             
@@ -54,13 +61,16 @@ public class GrindRailCalculator : MonoBehaviour
             Destination = NorthNode.transform.position + (LineVector * t);
             Skateboardrb.transform.position = Destination;
         }
+        //lets skateboard status know that the skateboard is on the grindrail, locked on to the grind rail and not in the air
         skateboardStatus.OnGrindRail = true;
         skateboardStatus.InAir = false;
         skateboardStatus.LockOn = true;
     }
     private void OnTriggerExit(Collider other)
     {
+        //clears the skateboard direction
         Direction = "";
+        //lets the skateboard know it is no longer on the ramp
         skateboardStatus.OnGrindRail = false;
         skateboardStatus.InAir = true;
     }
