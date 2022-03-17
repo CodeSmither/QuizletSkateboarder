92261338b0512fe40f6c93522d0383ba481282ce
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2022-03-17 15:47:19 +0000

    Update to Main Menu

--- Quizlet Skateboarder/Assets/Simon's Work/Scripts/GameController.cs
+++ Quizlet Skateboarder/Assets/Simon's Work/Scripts/GameController.cs
@@ -10,20 +10,28 @@ public class GameController : MonoBehaviour
     public Text Player2ScoreText;
     public Text Player1SpellingText;
     public Text Player2SpellingText;
+    // Different text settings for the UI;
     public float remaining_time = 100f;
+    // Remaining Time in the round
     public string wordInQuestion1 = "space";
     public string wordInQuestion2 = "space";
+    // Each players word that they have to spell
     public string player1spelling;
     public string player2spelling;
+    // Each players word current spelling of the world
     public float player1Score;
     public float player2Score;
+    // Each players current score
     public bool roundOver;
+    // Checks if the round is currently over
     public GameObject[] Nodes;
+    // stores all nodes needed
 
     private void Start()
     {
         StartCoroutine("Countdown");
         SelectNewWord();
+        //starts functions and coroutines which need to be checked in seconds rather than frames
     }
     private void Update()
     {
@@ -32,12 +40,13 @@ public class GameController : MonoBehaviour
         Player2ScoreText.text = player2Score.ToString();
         Player1SpellingText.text = player1spelling.ToUpper();
         Player2SpellingText.text = player2spelling.ToUpper();
-
+        // Updates all Text on the UI
     }
 
     public void WordChecking1()
     {   int x = player1spelling.Length + -1;
         char newestLetter = player1spelling[x];
+        // searches for the newest letter in the player spelling
         if(wordInQuestion1[x] != newestLetter)
         {
             player1Score += 50/ player1spelling.Length;
@@ -48,11 +57,14 @@ public class GameController : MonoBehaviour
             player1Score += 100;
             player1spelling = "";
         }
+        // checks if the letter used is the correct letter 
+        // then checks if the length of the spelled word is the same as the length of the stored word
     }
     public void WordChecking2()
     {
         int x = player2spelling.Length + -1;
         char newestLetter = player2spelling[x];
+        // searches for the newest letter in the player spelling
         if (wordInQuestion1[x] != newestLetter)
         {
             if (player2spelling.Length > 0)
@@ -66,19 +78,19 @@ public class GameController : MonoBehaviour
             player2Score += 100;
             player2spelling = "";
         }
-
+        // checks if the letter used is the correct letter 
+        // then checks if the length of the spelled word is the same as the length of the stored word
     }
     private void SelectNewWord()
     {
-        //Collect new String
-
+        //Collect new String 
         string tmpWord = "Space".ToUpper();
         char[] tmpWordarray = tmpWord.ToCharArray();
         for (int x = 0; x == 4; x++)
         {
             Nodes[x].name = tmpWordarray[x].ToString();
         }
-        
+        // Makes the string Upper case then randomly distributes each letter between each node
     }
     IEnumerator Countdown()
     {
@@ -92,5 +104,6 @@ public class GameController : MonoBehaviour
         {
             roundOver = true;
         }
+        // checks if the round is over or not
     }
 }
--- Quizlet Skateboarder/Assets/Simon's Work/Scripts/NodeLetter.cs
+++ Quizlet Skateboarder/Assets/Simon's Work/Scripts/NodeLetter.cs
@@ -5,36 +5,43 @@ using UnityEngine;
 public class NodeLetter : MonoBehaviour
 {
     public string letter;
-
+    //stores the letter that this node collected from the game controller
     public GameObject GameManager;
+    // stores a reference to the gameobject this is part of
     private bool cooling1;
     private bool cooling2;
+    // stores if a cooldown has been set for player 1 and 2
     public int objectCount;
+    // stores the current number of objects touching the point
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject == GameObject.FindGameObjectWithTag("Skateboard1") && cooling1 == false)
-        {
+        {// Checks if player 1 has entered the point and doesn't have a cooldown on their word
             
             GameManager.GetComponent<GameController>().player1spelling = GameManager.GetComponent<GameController>().player1spelling + letter;
             cooling1 = true;
             GameManager.GetComponent<GameController>().WordChecking1();
             StartCoroutine(CoolDownTimer());
+            // Adds Letter to player 1
         }
         if (other.gameObject == GameObject.FindGameObjectWithTag("Skateboard2") && cooling2 == false)
-        {
-            
+        {// Checks if player 2 has entered the point and doesn't have a cooldown on their word
+
             GameManager.GetComponent<GameController>().player2spelling = GameManager.GetComponent<GameController>().player2spelling + letter;
             cooling2 = true;
             GameManager.GetComponent<GameController>().WordChecking2();
             StartCoroutine(CoolDownTimer());
+            // Adds Letter to player 2
         }
         objectCount++;
+        //increases the object count
 
     }
     private void OnTriggerExit(Collider other)
     {
         objectCount--;
+        //decreases the object count when leaving the node
     }
 
     IEnumerator CoolDownTimer()
@@ -44,11 +51,13 @@ public class NodeLetter : MonoBehaviour
             yield return new WaitForSeconds(1f);
             cooling1 = false;
             cooling2 = false;
+            // turns cooldowns off after 1 second
         }
         else if(cooling1 == true || cooling2 == true)
         {
             yield return new WaitForSeconds(1f);
             StartCoroutine(CoolDownTimer());
+            // if cooldown hasn't been turned off but doesn't have the conditions to turn off it restarts the Coroutine
         }
     }
 
