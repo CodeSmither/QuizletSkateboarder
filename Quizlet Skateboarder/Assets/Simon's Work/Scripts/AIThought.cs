178c544fb65d5b774a2a2b1f5e757ee03aaaad52
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2022-03-25 23:27:19 +0000

    Quizlet AI Update

--- Quizlet Skateboarder/Assets/Simon's Work/Scripts/AIThought.cs
+++ Quizlet Skateboarder/Assets/Simon's Work/Scripts/AIThought.cs
@@ -10,6 +10,10 @@ public class AIThought : MonoBehaviour
     private GameController gameController;
     bool RoundPlaying = true;
     private string Objective = "E";
+    private string CurrentObjective;
+    [SerializeField] private GameObject[] Ramps;
+    [SerializeField] private GameObject[] TrickSpots;
+    [SerializeField] private GameObject[] GrindRailSpots;
     
     // Start is called before the first frame update
     void Start()
@@ -22,11 +26,49 @@ public class AIThought : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Objective = gameController.wordInQuestion2.Substring(gameController.player2spelling.Length,1);
+        
         
         if (RoundPlaying == true)
         {
-            agent.SetDestination(GameObject.Find(Objective.ToUpper()).transform.position);
+            switch (CurrentObjective)
+            {
+                case "Ramp":
+                    int rdnumber = Random.Range(0,Ramps.Length);
+                    Vector3 dest = Ramps[rdnumber].transform.position;
+                    if (dest != null)
+                    {
+                        agent.SetDestination(dest);
+                    }
+                    break;
+                case "Trick":
+                    int rdnumber2 = Random.Range(0,TrickSpots.Length);
+                    Vector3 dest2 = TrickSpots[rdnumber2].transform.position;
+                    if (dest2 != null)
+                    {
+                        agent.SetDestination(dest2);
+                    }
+                    break;
+
+                case "Grind":
+                    int rdnumber3 = Random.Range(0,GrindRailSpots.Length);
+                    Vector3 dest3 = GrindRailSpots[rdnumber3].transform.position;
+                    if (dest3 != null)
+                    {
+                        agent.SetDestination(dest3);
+                    }
+                    break;
+
+                case "Objective":
+                    Objective = gameController.wordInQuestion2.Substring(gameController.player2spelling.Length, 1);
+                    if (Objective != null)
+                    {
+                        agent.SetDestination(GameObject.Find(Objective.ToUpper()).transform.position);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
         }
         if (Vector3.zero != agent.velocity.normalized)
         {
